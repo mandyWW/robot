740e28f02f1933ee1ba533505a2e6becee4ddae3
Mandy Warren <mandys.inbox@gmail.com>
2018-06-03 21:35:16 +0100

    MW - renaming

---
+++ src/test/java/com/mandy/samples/InputReaderTest.java
@@ -0,0 +1,63 @@
+package com.mandy.samples;
+
+import com.mandy.samples.commands.CommandFactory;
+import com.mandy.samples.commands.PlaceCommand;
+import com.mandy.samples.exceptions.UnsupportedCommandException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.util.Scanner;
+
+import static org.mockito.Mockito.*;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({InputReader.class, Scanner.class, CommandFactory.class, PlaceCommand.class, UnsupportedCommandException.class})
+public class CommandReaderTest {
+
+    @Test
+    public void read_validInput() throws Exception {
+        // given
+        Robot mockedRobot = mock(Robot.class);
+        Scanner mockedScanner = PowerMockito.mock(Scanner.class);
+        PlaceCommand mockedPlaceCommand = mock(PlaceCommand.class);
+
+        PowerMockito.spy(PlaceCommand.class);
+
+        PowerMockito.whenNew(Robot.class).withNoArguments().thenReturn(mockedRobot);
+        PowerMockito.whenNew(PlaceCommand.class).withArguments(mockedRobot, 1, 1, Orientation.NORTH).thenReturn(mockedPlaceCommand);
+        PowerMockito.whenNew(Scanner.class).withArguments(System.in).thenReturn(mockedScanner);
+        when(mockedScanner.nextLine()).thenReturn("PLACE 1,1,NORTH").thenReturn("q");
+
+        // when
+        InputReader.read(System.in);
+
+        // then
+        PowerMockito.verifyNew(PlaceCommand.class).withArguments(mockedRobot, 1, 1, Orientation.NORTH);
+        verify(mockedPlaceCommand).execute();
+    }
+
+    @Test
+    public void read_invalidInput() throws Exception {
+        // given
+        Robot mockedRobot = mock(Robot.class);
+        Scanner mockedScanner = PowerMockito.mock(Scanner.class);
+        UnsupportedCommandException mockedException = mock(UnsupportedCommandException.class);
+
+        PowerMockito.spy(UnsupportedCommandException.class);
+
+        PowerMockito.whenNew(Robot.class).withNoArguments().thenReturn(mockedRobot);
+        PowerMockito.whenNew(UnsupportedCommandException.class).withArguments(anyString()).thenReturn(mockedException);
+        PowerMockito.whenNew(Scanner.class).withArguments(System.in).thenReturn(mockedScanner);
+        when(mockedScanner.nextLine()).thenReturn("rubbish").thenReturn("q");
+
+        // when
+        InputReader.read(System.in);
+
+        // then
+        PowerMockito.verifyNew(UnsupportedCommandException.class).withArguments("invalid instruction received");
+
+    }
+}
