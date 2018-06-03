03ba27b3d7d522b34b33ed31f475231bd616fada
Mandy Warren <mandys.inbox@gmail.com>
2018-06-03 19:45:06 +0100

    MW - initial creation to represent the board

--- src/main/java/com/mandy/samples/Board.java
+++ src/main/java/com/mandy/samples/Board.java
@@ -1,4 +1,26 @@
 package com.mandy.samples;
 
-public class Board {
+import org.apache.commons.lang3.Range;
+
+/**
+ * Represents a square board on which the robot moves.
+ *
+ * @author Mandy Warren
+ */
+final class Board {
+
+    private static final Range<Integer> XCOORD_RANGE = Range.between(0, 4);
+    private static final Range<Integer> YCOORD_RANGE = Range.between(0, 4);
+
+    /**
+     * Returns true if the xCoord and yCoord are in the valid range.
+     *
+     * @param xCoord the xCoord
+     * @param yCoord the yCoord
+     * @return true if the xCoord and yCoord are in the valid range
+     */
+    static boolean isValid(int xCoord, int yCoord) {
+        return XCOORD_RANGE.contains(xCoord) && YCOORD_RANGE.contains(yCoord);
+    }
+
 }
